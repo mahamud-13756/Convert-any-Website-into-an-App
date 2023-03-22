c9184b256150367b3f6188e437befdf42322257a
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2023-03-23 01:55:12 +0600

    now it is working perfectly

--- app/src/main/java/com/example/convertanywebsiteintoanapp/MainActivity.java
+++ app/src/main/java/com/example/convertanywebsiteintoanapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +17,11 @@ public class MainActivity extends AppCompatActivity {
 
         webView = findViewById(R.id.webView);
 
+
+
         webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient()); // now everything is open in app
 
         webView.loadUrl("https://tuitionterminal.com.bd/");
     }
