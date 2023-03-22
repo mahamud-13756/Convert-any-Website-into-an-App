17c5c28381a41d84df9c92dc90fef14954023d7b
Mahamud_Mredha <mahamud15-13756@diu.edu.bd>
2023-03-23 02:28:26 +0600

    first page design done and coppy mainActivity and pase and setContentView ;ink_input

---
+++ app/src/main/java/com/example/convertanywebsiteintoanapp/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.convertanywebsiteintoanapp;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.link_input);
+
+
+    }
+}
